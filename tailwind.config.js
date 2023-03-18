7f88f4a54ec12fa1f6e0448f62a601679c8596d3
saleh alsaggaf <saa111954@hotmail.com>
2023-03-18 22:40:02 +0300

    update: added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      p1: "#3F3B6C",
+      p2: "#A3C7D6",
+      p3: "#9F73AB",
+      p4: "#624F82",
+      s1: "#D5B1DF",
+      t1: "#D0C7D2",
+      s2: "#2B2757",
+      s3: "#1D1A41",
+      w1: "#FFFFFF",
+    },
+    extend: {
+      
+    },
     themes: ["cupcake", "dark", "cmyk"],
   
 
