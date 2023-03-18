6946515288457269c8506964b2058cb175b27b8a
saleh alsaggaf <saa111954@hotmail.com>
2023-03-18 04:03:02 +0300

    update: added level field for all models, because Moe love level field

--- src/types/lesson.type.ts
+++ src/types/lesson.type.ts
@@ -8,14 +8,14 @@ export const LessonSchema = z.object({
   }),
   subject_id: z.string({ required_error: "subject_id is required" }).uuid(),
   status: z.boolean().default(false),
-  level: z.number(),
+  level: z.number().default(0).optional()
 });
 export const LessonUpdateDTOSchema = LessonSchema.extend({
   id: z.string().uuid().optional(),
   title: z.string().optional(),
   subject_id: z.string().uuid().optional(),
   status: z.boolean().default(false).optional(),
-  level: z.number().optional(),
+  level:z.number().default(0).optional()
 });
 export type Lesson = z.infer<
   typeof LessonSchema | typeof LessonUpdateDTOSchema
